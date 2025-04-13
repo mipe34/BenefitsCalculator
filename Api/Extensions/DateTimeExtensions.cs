154b8b2f4621cf7e0cdcdbfb7e55abc589832314
mipe34 <petrik.mich@gmail.com>
2025-04-13 23:10:36 +0200

    datetimeextensions unit tests

--- Api/Extensions/DateTimeExtensions.cs
+++ Api/Extensions/DateTimeExtensions.cs
@@ -4,14 +4,14 @@
     {
         public static decimal PortionOfYearTurnedSelectedAge(this DateTime birthDay, DateTime toDate, int ageDiscriminator)
         {
-            var bornBeforeCutline = toDate.AddYears(-ageDiscriminator);
+            var bornBeforeCutline = toDate.AddYears(-ageDiscriminator).Year;
             // not applicable
-            if (birthDay > bornBeforeCutline) return 0;         
-            // turned tha age just in the year of the cut date - count months of that year
-            if (birthDay.Year == bornBeforeCutline.Year)
+            if (birthDay.Year > bornBeforeCutline) return 0;         
+            // turned tha age just in the year of the cut date - count portion of that year
+            if (birthDay.Year == bornBeforeCutline)
             {
-                var numberOfYearDays = DateTime.IsLeapYear(toDate.Year) ? 366 : 365;
-                return birthDay.DayOfYear / numberOfYearDays;
+                var numberOfYearDays = DateTime.IsLeapYear(toDate.Year) ? 366m : 365m;
+                return (numberOfYearDays - birthDay.DayOfYear + 1) / numberOfYearDays;
             }
             // turned the age year or more before the cut date
             return 1m;
---
+++ ApiTests/UnitTests/DateTimeExtensionsUnitTests.cs
@@ -0,0 +1,56 @@
+﻿using Api.Extensions;
+using System;
+using Xunit;
+
+namespace ApiTests.UnitTests
+{
+    public class DateTimeExtensionsUnitTests
+    {
+        [Fact]
+        public void PortionOfYearTurnedSelectedAge_ReturnsZeroWhenAgeNotReached()
+        {
+            //arrange
+            var birthDay = new DateTime(2000, 01, 01);
+            var toDate = new DateTime(2025, 01, 01);
+            var age = 50;
+
+            //act
+            var result = birthDay.PortionOfYearTurnedSelectedAge(toDate, age);
+
+            //assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void PortionOfYearTurnedSelectedAge_ReturnsOneWhenAgeReachedMoreThanAYearBefore()
+        {
+            //arrange
+            var birthDay = new DateTime(1950, 01, 01);
+            var toDate = new DateTime(2025, 01, 01);
+            var age = 50;
+
+            //act
+            var result = birthDay.PortionOfYearTurnedSelectedAge(toDate, age);
+
+            //assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void PortionOfYearTurnedSelectedAge_ReturnsCorrectPortionOfYearWhenJustTurnedTheAge()
+        {
+            //arrange
+            var birthDay = new DateTime(1975, 12, 31);
+            var toDate = new DateTime(2025, 01, 01);
+            var age = 50;
+
+            //act
+            var result = birthDay.PortionOfYearTurnedSelectedAge(toDate, age);
+
+            //assert
+            Assert.Equal(1m/365m, result);
+        }
+
+        //TODO extend test coverage
+    }
+}
